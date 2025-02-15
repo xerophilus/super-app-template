af77872b31d29651a0c4f63ae94c257190d4f186
Ben Pitts <bwilliamp@bens-air.mynetworksettings.com>
2025-02-15 08:32:08 -0500

    fixing the bug

--- micro-apps/micro-app-four/babel.config.js
+++ micro-apps/micro-app-four/babel.config.js
@@ -2,9 +2,15 @@ module.exports = {
   presets: [
     '@babel/preset-react',
     '@babel/preset-typescript',
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-env', { 
+      targets: { node: 'current' },
+      modules: 'commonjs',
+      useBuiltIns: 'usage',
+      corejs: 3
+    }],
   ],
   plugins: [
     ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
+    '@babel/plugin-transform-runtime'
   ],
 }; 
